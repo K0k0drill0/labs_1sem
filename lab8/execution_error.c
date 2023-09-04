fe695c362bfbd4059378d01b8627c7c905b9facb
Anastasia Astakhova <69476057+K0k0drill0@users.noreply.github.com>
2023-09-04 22:35:41 +0300

    Create execution_error.c

---
+++ lab8/execution_error.c
@@ -0,0 +1,14 @@
+/*
+Астахова Анастасия Сергеевна, группа М8О-113Б-22
+Ошибка при выполнении.
+*/
+
+#include <stdio.h>
+int main() {
+	float a, b;
+	scanf("%f%f", &a, &b);
+	
+	float x = -b / a;
+	printf("%f\n", x);
+	return 0;
+}
